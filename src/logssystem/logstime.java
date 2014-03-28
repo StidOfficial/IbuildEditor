34601edb8008eacfecd10f3400cbdb141d9a4b96
StidOfficial <aucun@gmail.com>
2014-03-28 20:56:47 +0100

    Update files

---
+++ src/logssystem/logstime.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package logssystem;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Properties;
+
+/**
+ *
+ * @author quentin
+ */
+public class logstime {
+
+    
+}
